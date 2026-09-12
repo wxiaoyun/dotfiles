4dc949a04cefe0ca12ee7d0c53de2bd2b9ff13c9
wxiaoyun <w.xy020203@gmail.com>
2026-09-12 14:19:26 +0800

    feat: slim bash and align PATH

---
+++ dotfiles/config/shell/env.sh
@@ -0,0 +1,13 @@
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
+export LANG=en_US.UTF-8
+export EDITOR=nvim
+export MANPAGER="nvim +Man!"
+if GPG_TTY=$(tty 2>/dev/null); then
+  export GPG_TTY
+fi
+
+export PATH="$HOME/.local/bin:$PATH"
